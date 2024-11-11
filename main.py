0b5cff5d95d567a1459d707926bbe92c2d93c740
vlad4 <vlad4.efremova@yandex.ru>
2024-11-11 16:48:51 +0300

    changes in classes

--- main.py
+++ main.py
@@ -13,20 +13,25 @@ class Event:
                 f", price={self._price}")
 
 class SportEvent(Event):
-    def __init__(self, idd, event_name, date, location, price, tickets, seats):
+    def __init__(self, idd, event_name, date, location, price, tickets, seats, sector, type_of_sport):
         Event.__init__(self, idd, event_name, date, location, price, tickets, seats)
-
+        self._sector = sector
+        self._type_of_sport = type_of_sport
 
 class Concert(Event):
-    def __init__(self, idd, event_name, date, location, price, tickets, seats):
+    def __init__(self, idd, event_name, date, location, price, tickets, seats, artist, genre_of_music):
         Event.__init__(self, idd, event_name, date, location, price, tickets, seats)
+        self._genre_of_music = genre_of_music
+        self._artist = artist
 
 class Cinema(Event):
-    def __init__(self, idd, event_name, date, location, price, tickets, seats):
+    def __init__(self, idd, event_name, date, location, price, tickets, seats, forms, age_limit):
         Event.__init__(self, idd, event_name, date, location, price, tickets, seats)
+        self._forms = forms
+        self._age_limit = age_limit
 
 class Theatre(Event):
-    def __init__(self, idd, event_name, date, location, price, tickets, seats):
+    def __init__(self, idd, event_name, date, location, price, tickets, seats, genre, cast):
         Event.__init__(self, idd, event_name, date, location, price, tickets, seats)
-
-
+        self._genre = genre
+        self._cast = cast
